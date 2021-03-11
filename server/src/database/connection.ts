d636c1d99f531fa5c4126c26cbfed9425a85f0ff
andreocunha <andreoliveiracunha20@gmail.com>
2021-03-11 00:54:54 -0300

    backend, web e mobile

---
+++ server/src/database/connection.ts
@@ -0,0 +1,16 @@
+// Arquivo responsável pela conexão com o banco de dados
+
+import knex from 'knex';
+import path from 'path'
+
+const connection = knex({
+    client: 'sqlite3',
+    connection:{
+        filename:path.resolve(__dirname, 'database.sqlite'),
+    },
+    useNullAsDefault: true,
+});
+
+export default connection;
+
+// Migrations = Histórico do banco de dados
